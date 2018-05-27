6dae7817054ac5eb62168b3bbd306c756b121ced
september <zzzhen1994@gmail.com>
2018-05-27 19:49:52 +0800

    chore:清理文件

---
+++ json/json.go
@@ -0,0 +1,22 @@
+// +build !jsoniter
+
+package json
+
+import "encoding/json"
+
+var (
+	Marshal       = json.Marshal
+	Unmarshal     = json.Unmarshal
+	MarshalIndent = json.MarshalIndent
+	NewDecoder    = json.NewDecoder
+	NewEncoder    = json.NewEncoder
+	Valid         = json.Valid
+	Compact       = json.Compact
+	HTMLEscape    = json.HTMLEscape
+	Indent        = json.Indent
+)
+
+type (
+	Number = json.Number
+	RawMessage = json.RawMessage
+)
---
+++ json/jsoniter.go
@@ -0,0 +1,27 @@
+// +build jsoniter
+
+package json
+
+import (
+	stdjson "encoding/json"
+
+	"github.com/json-iterator/go"
+)
+
+var (
+	json          = jsoniter.ConfigCompatibleWithStandardLibrary
+	Unmarshal     = json.Unmarshal
+	Marshal       = json.Marshal
+	MarshalIndent = json.MarshalIndent
+	NewDecoder    = json.NewDecoder
+	NewEncoder    = json.NewEncoder
+	Valid         = json.Valid
+	Compact       = stdjson.Compact
+	HTMLEscape    = stdjson.HTMLEscape
+	Indent        = stdjson.Indent
+)
+
+type (
+	Number = jsoniter.Number
+	RawMessage = jsoniter.RawMessage
+)
